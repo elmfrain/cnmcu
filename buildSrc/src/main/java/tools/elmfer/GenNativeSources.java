29e0876acd520330f4f3dbe0ccc24b106a61ac38
elmfrain <elmfer10@gmail.com>
2024-04-30 03:19:13 -0500

    Use appropriate path separator for OS

--- buildSrc/src/main/java/tools/elmfer/GenNativeSources.java
+++ buildSrc/src/main/java/tools/elmfer/GenNativeSources.java
@@ -19,8 +19,10 @@ public class GenNativeSources extends DefaultTask {
     private String bridgeDir;
     
     public GenNativeSources() {
+        String pathSeparator = getOS().equals("windows") ? ";" : ":";
+        
         classPath = getProject().getConfigurations().getByName("runtimeClasspath").getAsPath() +
-                ":build/classes/java/main";
+                pathSeparator + "build/classes/java/main";
         
         this.setGroup(GROUP);
         this.setDescription(DESCRIPTION);
@@ -52,5 +54,17 @@ public class GenNativeSources extends DefaultTask {
 
     public void setBridgeDir(String bridgeDir) {
         this.bridgeDir = bridgeDir;
+    }
+    
+    private String getOS() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win"))
+            return "windows";
+        else if (os.contains("mac"))
+            return "macos";
+        else if (os.contains("nix") || os.contains("nux"))
+            return "linux";
+
+        return "unknown";
     }
 }
