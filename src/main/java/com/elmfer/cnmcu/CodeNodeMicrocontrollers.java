764e98af77f540e0b01c95cefcea675e94215793
elmfrain <elmfrain@users.noreply.github.com>
2024-05-05 13:01:36 +0000

    Update mod version to 0.0.8a-1.20.4

--- src/main/java/com/elmfer/cnmcu/CodeNodeMicrocontrollers.java
+++ src/main/java/com/elmfer/cnmcu/CodeNodeMicrocontrollers.java
@@ -18,7 +18,7 @@ public class CodeNodeMicrocontrollers implements ModInitializer {
 
     public static final String MOD_NAME = "CodeNode Microcontrollers";
     public static final String MOD_ID = "cnmcu";
-    public static final String MOD_VERSION = "0.0.7a-1.20.4";
+    public static final String MOD_VERSION = "0.0.8a-1.20.4";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
